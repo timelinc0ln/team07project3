8e80f7c6eb8a062f2c0740a62314f236a614ab94
Cullen Brown <Cullen@nat-10-200-59-3.tamulink.tamu.edu>
2013-04-22 14:39:27 -0500

    Windows and stuff
    here is some stuff from my latest attempts at using Tkinter

--- CalendarTest/CalendarReader/CalReader.py
+++ CalendarTest/CalendarReader/CalReader.py
@@ -5,6 +5,9 @@ Created on Apr 18, 2013
 '''
 import gdata.docs.service
 import gdata.calendar.service
+from Tkinter import Tk, BOTH, Entry, Message
+from ttk import Frame, Button, Style
+
 
 client = gdata.docs.service.DocsService()
 client.ClientLogin('project3team07@gmail.com', 'teamseven')
@@ -21,15 +24,16 @@ calendar_feed = client2.GetCalendarListFeed()
 for calendar_list_entry in calendar_feed.entry:
     
     if calendar_list_entry.title.text == "Test Calendar":
-       # print(calendar_list_entry.title.text)
-        calendar_event_feed = client2.GetCalendarEventFeed(calendar_list_entry.GetCalendarListEntryUri())
-        for calendar_event_list_entry in calendar_event_feed.entry:
-            print(calendar_event_list_entry.title.text)
-        print("I found it")
+       print(calendar_list_entry.title.text)
+       #calendar_event_feed = calendar_list_entry.GetCalendarEventFeed()
+        #for calendar_event_list_entry in calendar_event_feed.entry:
+            #print(calendar_event_list_entry.title.text)
+       print("I found it")
 
 #comments and stuff
 #comments
 #comments
+<<<<<<< HEAD
 
 calendar_feed = client2.GetAllCalendarsFeed()
 #print(calendar_feed)
@@ -37,6 +41,52 @@ calendar_feed = client2.GetAllCalendarsFeed()
 client2.CalendarClient.InsertEvent()
 
 
+=======
+class Example(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.initUI()
+    def initUI(self):
+        self.parent.title("window title")
+        self.style = Style()
+        self.style.theme_use("default")
+        self.pack(fill=BOTH, expand = 1)
+        
+        quitButton = Button(self,text="Quit", command=self.quit)
+        quitButton.place(x=50, y=50)
+        v = None
+        
+        m = Message(self, textvariable = v)
+        m.place(x = 25, y = 25)
+        
+        myEntry = Entry(self, textvariable = v, command=self.update())
+        myEntry.place (x = 150, y = 50)
+        
+        p = None
+        
+        
+        
+       
+        
+        printButton = Button(self,text="Print")
+        printButton.place(x = 400, y = 50)
+        
+        
+>>>>>>> Windows and stuff
 
 for document_entry in documents_feed.entry:
     print(document_entry.title.text)
+    
+    
+    
+def main():
+    
+    root = Tk()
+    root.geometry("600x200+300+300")
+    app = Example(root)
+    root.mainloop()
+
+    
+if __name__ == '__main__':
+    main()
