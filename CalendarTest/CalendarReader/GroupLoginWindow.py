3dbba1081ee5f2fea4c4f1d1d3e19f2f74749dd2
Cullen Brown <Cullen@nat-10-200-43-34.tamulink.tamu.edu>
2013-04-25 21:16:10 -0500

    Widgets for group login made

--- CalendarTest/CalendarReader/GroupLoginWindow.py
+++ CalendarTest/CalendarReader/GroupLoginWindow.py
@@ -21,18 +21,39 @@ class GroupLoginWindow(Frame):
     def initUI(self):
         #define widgets
         #widgets for "NEW" group side
-        self.newGroupHeader = Label(self, text="Register A New Group", underline=1)
-        self.newGroupName = Entry(self, textvariable=self.newGroupNameString)
-        self.newGroupPass = Entry(self, textvariable=self.newGroupPassString)
-        self.newGroupConfirm = Entry(self, textvariable=self.newGroupConfirmString)
-        self.nGNPrompt = Label(self, text="Group Name:")
-        self.nGPPrompt = Label(self, text="Password:")
-        self.nGCPrompt = Label(self, text="Confirm Password:")
-        
+        self.newHeader = Label(self, text="Register A New Group", underline=1)
+        self.newName = Entry(self, textvariable=self.newNameString)
+        self.newPass = Entry(self, textvariable=self.newPassString)
+        self.newConfirm = Entry(self, textvariable=self.newConfirmString)
+        self.newNamePrompt = Label(self, text="Group Name:")
+        self.newPassPrompt = Label(self, text="Password:")
+        self.newConfirmPrompt = Label(self, text="Confirm Password:")
+        self.registerButton = Button(self, text="Register Group", command=lambda: self.callBack("Button","Register"))
         
         #widgets for "EXISTING" group side
-        self.existGroupName = Entry(self, textvariable=self.existGroupNameString)
-        self.existGroupPass= Entry(self, textvariable=self.existGroupPass)
+        self.existHeader = Label(self, text="Access An Existing Group", underline=1)
+        self.existName = Entry(self, textvariable=self.existNameString)
+        self.existPass= Entry(self, textvariable=self.existPassString)
+        self.existNamePrompt = Label(self, text="Group Name:")
+        self.existPassPrompt = Label(self, text="Password:")
+        self.loginButton = Button(self, text="Login", command=lambda: self.callBack("Button","Login"))
+        
+        #widget to quit
+        self.quitButton = Button(self, text="Quit", command=lambda: self.callBack("Button","Quit"))
+        
+        #design window
+        
+        
+    def callBack(self, callerType, callerName):
+        #Buttons
+        if callerType == "Button":
+            if callerName == "Register":
+                print("Register clicked")
+            elif callerName == "Login":
+                print("Login clicked")
+            elif callerName == "Quit":
+                print("Exiting")
+       
     
         
         
